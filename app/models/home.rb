d0c56775d073fe6fb47f4de5bb60561ea8ce18e3
unknown <carmenhchung@gmail.com>
2017-10-25 21:50:29 +1100

    added default image for homes - will appear if no image is uploaded by user.

--- app/models/home.rb
+++ app/models/home.rb
@@ -2,4 +2,13 @@ class Home < ApplicationRecord
   belongs_to :user
   has_one :photos
   include ImageUploader[:image]
+
+  def home_image
+    if self.image.nil?
+      "default_home.svg"
+    else
+      self.image.url
+    end
+    # If there is a photo for the home, show it, otherwise show a default home picture.
+  end
 end
