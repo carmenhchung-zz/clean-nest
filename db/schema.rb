a2e00b0484a197fc3d33737818fc1728008ee96f
carmenhchung <carmenhchung@gmail.com>
2017-10-30 14:12:00 +1100

    switch to postgres database (now using Mac)

--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028062536) do
+ActiveRecord::Schema.define(version: 20171016114822) do
 
-  create_table "appointments", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "home_id"
-    t.datetime "date"
-    t.datetime "time"
-    t.integer "hours"
-    t.integer "hourly_price"
-    t.integer "total"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["home_id"], name: "index_appointments_on_home_id"
-    t.index ["user_id"], name: "index_appointments_on_user_id"
-  end
-
-  create_table "homes", force: :cascade do |t|
-    t.string "home_type"
-    t.string "listing_name"
-    t.text "address"
-    t.text "instructions"
-    t.integer "hours"
-    t.integer "hourly_rate"
-    t.integer "user_id"
-    t.boolean "supplies_provided"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text "image_data"
-    t.float "latitude"
-    t.float "longitude"
-    t.index ["user_id"], name: "index_homes_on_user_id"
-  end
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -62,11 +34,6 @@ ActiveRecord::Schema.define(version: 20171028062536) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string "phone_number"
-    t.string "gender"
-    t.string "language"
-    t.text "location"
-    t.text "bio"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
