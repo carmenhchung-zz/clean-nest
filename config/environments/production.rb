dc22ee7bc1060df24604c57cc53abab45b4d2501
carmenhchung <carmenhchung@gmail.com>
2017-11-02 23:16:32 +1100

    add digest to production.rb to make images show on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
